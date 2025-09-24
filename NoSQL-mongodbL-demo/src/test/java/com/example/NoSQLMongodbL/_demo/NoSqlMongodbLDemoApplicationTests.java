3b61f42ee8601e2af1d7ac9a87f1c634115627fa
Anna Zhevlakova <azevlakova12@gmail.com>
2025-09-24 20:21:49 +0400

    Start

---
+++ NoSQL-mongodbL-demo/src/test/java/com/example/NoSQLMongodbL/_demo/NoSqlMongodbLDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NoSQLMongodbL._demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoSqlMongodbLDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
